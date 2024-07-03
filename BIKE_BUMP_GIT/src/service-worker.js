888435d1eb58eba8ca57922f37629d8d76b4f06d
Quadricepz <simon.rampeltshammer@hs-augsburg.de>
2024-07-03 22:00:19 +0200

    0.7.3

--- BIKE_BUMP_GIT/src/service-worker.js
+++ BIKE_BUMP_GIT/src/service-worker.js
@@ -1,19 +1,68 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('bike-bump-cache').then(function(cache) {
-        return cache.addAll([
-          '/START.html',
-          '/Assets/BIKE_BUMP.ico',
-          // Add other resources to cache
-        ]);
+const CACHE_NAME = 'bike-bump-cache-v1';
+const urlsToCache = [
+  'START.html',
+  '/Assets/BIKE_BUMP.ico',
+  '/Assets/BIKE_BUMP.png',
+  '/Assets/BIKE_BUMP_180.png',
+  '/Assets/logoRad.png',
+  '/Assets/logoSchrift.png',
+  // Add any other resources you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       })
-    );
-  });
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
